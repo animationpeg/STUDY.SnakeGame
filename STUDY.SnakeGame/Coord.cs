73b7291199b2efee9d84d7e73c6dd25d75e63847
animationpeg <jeffers800@hotmail.co.uk>
2026-01-12 12:24:38 +0000

    Initialised project. Created a Coord class to track the grid playinng area. Main program prints the playing field visually to the console.

---
+++ STUDY.SnakeGame/Coord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STUDY.SnakeGame
+{
+    internal class Coord
+    {
+        private int x;
+        private int y;
+
+        public int X { get { return x; } }
+        public int Y { get { return y; } }
+
+        // Class Constructor
+        public Coord(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ STUDY.SnakeGame/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STUDY.SnakeGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Coord gridDimensions = new Coord(50, 20);
+            Coord snakePos = new Coord(10, 0);
+
+            // Create playing field
+            for (int y = 0; y < gridDimensions.Y; y++)
+            {
+                for (int x = 0; x < gridDimensions.X; x++)
+                {
+                    if (x == 0 || y == 0 || x == gridDimensions.X - 1 || y == gridDimensions.Y - 1)
+                        Console.Write("#");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
